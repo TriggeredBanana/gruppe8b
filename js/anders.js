2f1d269fbb3c6268c269894358728e86f66058af
Anders Fløysvik <andf7@hotmail.com>
2023-11-21 22:10:25 +0100

    Fullført Canvas API Anders

--- js/anders.js
+++ js/anders.js
@@ -6,3 +6,6 @@ import functions from "./figures.js";
 
 functions.ellipse(100, 100, 80, 90, 0, 0, "lightgreen", "canvasA");
 functions.circle(60, 75, 30, "white", "canvasA");
+functions.circle(140, 75, 30, "white", "canvasA");
+functions.ellipse(100, 150, 40, 20, 0, 0, "red", "canvasA");
+functions.ellipse(100, 150, 30, 10, 0, 0, "lightcoral", "canvasA");
