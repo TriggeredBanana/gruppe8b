dc978c21d6cf5a5c6e8cc90b6c3a3b5e97732b74
Anders Fløysvik <andf7@hotmail.com>
2023-11-22 00:04:20 +0100

    Videre utforming

--- js/figures.js
+++ js/figures.js
@@ -33,19 +33,19 @@ const functions = {
         circle.fillStyle = color;
         circle.fill();
     },
-      
-      // Lagrer funksjonen for rektangel i en variabel 
-triangle: function (x, y, x1, y1, x2, y2, color, name) {
-    var canvas = document.getElementById(name);
-    var triangle = canvas.getContext("2d");
 
-    triangle.beginPath();
-    triangle.moveTo(x, y);
-    triangle.lineTo(x1, y1);
-    triangle.lineTo(x2, y2);
-    triangle.fillStyle = color;
-    triangle.fill();
-},
+    // Lagrer funksjonen for rektangel i en variabel 
+    triangle: function (x, y, x1, y1, x2, y2, color, name) {
+        var canvas = document.getElementById(name);
+        var triangle = canvas.getContext("2d");
+
+        triangle.beginPath();
+        triangle.moveTo(x, y);
+        triangle.lineTo(x1, y1);
+        triangle.lineTo(x2, y2);
+        triangle.fillStyle = color;
+        triangle.fill();
+    },
 };
 
 export default functions;
